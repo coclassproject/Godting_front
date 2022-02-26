62f07e29e33f293d0228d15eda7177019b60874b
JhsK <qwe6293@nate.com>
2022-02-26 22:01:43 +0900

    setting

---
+++ src/theme/globalStyle.tsx
@@ -0,0 +1,38 @@
+import { Global, css } from '@emotion/react';
+import reset from 'emotion-reset';
+import React from 'react';
+
+// interface GlobalStyleProps {
+//   theme: ThemeType;
+// }
+
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      ${reset} html, body, #__next {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+      }
+
+      body {
+        overflow-x: hidden;
+        width: 100%;
+        /* background-color: ${lightTheme.PUBLIC_WHITE}; */
+        font-family: NanumSquare, 'Noto Sans KR', notokr, 'Nanum Gothic', 'Malgun Gothic', sans-serif;
+      }
+
+      a {
+        text-decoration: none;
+        outline: none;
+        /* color: ${lightTheme.PUBLIC_BLACK}; */
+      }
+
+      button {
+        border: 1px solid #000;
+      }
+    `}
+  />
+);
+
+export default GlobalStyle;
