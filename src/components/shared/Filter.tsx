7fc0635bf7140c730337e6170252a322364c2613
JhsK <qwe6293@nate.com>
2022-03-18 10:45:27 +0900

    [#3] 컴포넌트 틀 작성

---
+++ src/components/shared/Filter.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  width: 100%;
+  height: 500px;
+  position: absolute;
+  bottom: 0px;
+  z-index: 999;
+`;
+const Filter = () => <Container>hsd</Container>;
+
+export default Filter;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import Filter from 'src/components/shared/Filter';
 import Layout from 'src/components/shared/Layout';
 
-const Home = () => <Layout />;
+const Home = () => (
+  <Layout>
+    <h1>sd</h1>
+    <Filter />
+  </Layout>
+);
 
 export default Home;
