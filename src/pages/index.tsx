d9ac725b510a5da336ad54142302ef631567a573
duck <godqhr2256@gmail.com>
2022-03-27 00:08:28 +0900

    [#7] Register 화면 작업~ing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Filter from 'src/components/shared/Filter';
 import Layout from 'src/components/shared/Layout';
 import Main from 'src/components/home/Main';
+import Register from 'src/components/Register';
 
 const Home = () => (
   <Layout>
-    <Main />
+    <Register />
   </Layout>
 );
 
