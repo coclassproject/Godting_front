49d79f19ffe777980862e667978c630365bcf896
JhsK <qwe6293@nate.com>
2022-04-13 20:37:07 +0900

    [#2] 메인 페이지 구조 변경 및 필터 컴포넌트 팝업 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,12 @@ import Filter from 'src/components/shared/Filter';
 import Layout from 'src/components/shared/Layout';
 import Main from 'src/components/home/Main';
 import Register from 'src/components/Register';
+import HomeComponent from 'src/components/home';
 
 const Home = () => (
   <Layout>
     {/* <Register /> */}
-    <Filter />
+    <HomeComponent />
   </Layout>
 );
 
