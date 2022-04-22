1032cde81c18e773df85b53f468936e2d2f3b308
JhsK <qwe6293@nate.com>
2022-04-22 15:20:44 +0900

    deploy bug

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -7,7 +7,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import Filter from '../shared/filter';
+import Filter from '../shared/Filter';
 
 const Container = styled.div`
   width: 100%;
